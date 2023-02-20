6382dd2c049fdc30b69e158bd990a6798df4db1d
Atul Singh <atulsinghsrj@gmail.com>
2023-02-20 17:22:28 +0530

    dom class 2 code

---
+++ classCodePracticeByMe/DOM_class_2nd/events.js
@@ -0,0 +1,11 @@
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", function () {
+    document.getElementsByTagName("button")[0].innerHTML = "text";
+  });
+
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "red";
+  });
