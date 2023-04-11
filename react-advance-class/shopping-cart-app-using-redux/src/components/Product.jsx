e27d9c03377028a39d7b7854a89758f0a35d8c6e
Atul Singh <atulsinghsrj@gmail.com>
2023-04-11 13:55:32 +0530

    spinner color changed

--- react-advance-class/shopping-cart-app-using-redux/src/components/Product.jsx
+++ react-advance-class/shopping-cart-app-using-redux/src/components/Product.jsx
@@ -68,3 +68,48 @@ const Product = ({ item }) => {
 };
 
 export default Product;
+/////////////////////////////////////////////////
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { toast } from "react-hot-toast";
+// import { add, remove } from "../redux/Slices/CartSlice";
+
+// const Product = ({ post }) => {
+//   const { cart } = useSelector((state) => state);
+//   const dispatch = useDispatch();
+
+//   const addToCart = () => {
+//     dispatch(add(post));
+//     toast.success("item added to cart");
+//   };
+
+//   const removeFromCart = () => {
+//     dispatch(remove(post.id));
+//     toast.error("item remove from cart");
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <p>{post.title}</p>
+//       </div>
+//       <div>
+//         <p>{post.description.split(" ").slice(0, 10).join(" ") + "..."}</p>
+//       </div>
+//       <div>
+//         <img src={post.image} />
+//       </div>
+//       <div>
+//         <p>{post.price}</p>
+//       </div>
+
+//       {cart.some((p) => p.id == post.id) ? (
+//         <button onClick={removeFromCart}>Remove Item</button>
+//       ) : (
+//         <button onClick={addToCart}>Add to Cart</button>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Product;
--- react-advance-class/shopping-cart-app-using-redux/src/pages/Home.jsx
+++ react-advance-class/shopping-cart-app-using-redux/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       const res = await fetch(API_URL);
       const data = await res.json();
       setPosts(data);
-      // console.log(data);
+      console.log(data);
     } catch (error) {
       console.log("error aa gya  api se" + error);
       setPosts([]);
@@ -49,3 +49,54 @@ const Home = () => {
 };
 
 export default Home;
+/////////////////////////////////////////////////////
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import Spinner from "../components/Spinner";
+// import Product from "../components/Product";
+
+// const Home = () => {
+//   const API_URL = "https://fakestoreapi.com/products";
+//   const [loading, setLoading] = useState(false);
+//   const [posts, setPosts] = useState([]);
+
+//   async function fetchProductData() {
+//     setLoading(true);
+
+//     try {
+//       const res = await fetch(API_URL);
+//       const data = await res.json();
+//       setPosts(data);
+//       console.log(data);
+//     } catch {
+//       console.log("error agya");
+//       setPosts([]);
+//     }
+
+//     setLoading(false);
+//   }
+
+//   useEffect(() => {
+//     fetchProductData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {loading ? (
+//         <Spinner />
+//       ) : posts.length > 0 ? (
+//         <div>
+//           {posts.map((post) => {
+//             return <Product key={post.id} post={post} />;
+//           })}
+//         </div>
+//       ) : (
+//         <div>
+//           <p>No data found</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Home;
