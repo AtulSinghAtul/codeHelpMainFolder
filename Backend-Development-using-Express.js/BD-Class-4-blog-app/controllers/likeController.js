3b80e7e16c5560e4813080c4ba30f8ad8effa41f
Atul Singh <atulsinghsrj@gmail.com>
2023-05-05 21:43:58 +0530

    write like and unlike logic

--- Backend-Development-using-Express.js/BD-Class-4-blog-app/controllers/likeController.js
+++ Backend-Development-using-Express.js/BD-Class-4-blog-app/controllers/likeController.js
@@ -12,6 +12,7 @@ exports.likePost = async (req, res) => {
 
     const savedLike = likeObject.save();
 
+    // update the post collection basis on this
     const updatedPost = Post.findByIdAndUpdate(
       post,
       {
@@ -27,6 +28,32 @@ exports.likePost = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       err: "error like",
+    });
+  }
+};
+
+// Unlike a post
+exports.unlikePost = async (req, res) => {
+  try {
+    const { post, like } = req.body;
+    //find and delete the like collection me se
+    const deletedLike = await Like.findOneAndDelete({ post: post, _id: like });
+
+    //update the post collection
+    const updatedPost = await Post.findByIdAndDelete(
+      post,
+      {
+        $pull: { like: deletedLike._id },
+      },
+      { new: true }
+    );
+
+    res.json({
+      post: updatedPost,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: "Error while Unliking post",
     });
   }
 };
